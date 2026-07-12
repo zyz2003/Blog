d7d0a0dc9158b9d301912877515c368fa9ce6453
ZhangYaZhou <2163447956@qq.com>
2026-07-12 17:19:01 +0800

    feat(08-04): wire AlbumModule with controllers
    - Add AlbumController, AlbumCategoryController, PublicAlbumController
    - Imports: DatabaseModule, PostTagModule, ThumbnailModule (forwardRef)
    - Exports: AlbumService, AlbumCategoryService, AlbumRepository, AlbumCategoryRepository

--- server/src/album/album.module.ts
+++ server/src/album/album.module.ts
@@ -4,6 +4,9 @@ import { AlbumService } from './album.service';
 import { AlbumCategoryService } from './album-category.service';
 import { AlbumRepository } from './album.repository';
 import { AlbumCategoryRepository } from './album-category.repository';
+import { AlbumController } from './album.controller';
+import { AlbumCategoryController } from './album-category.controller';
+import { PublicAlbumController } from './public-album.controller';
 import { PostTagModule } from '../post-tag/post-tag.module';
 import { ThumbnailModule } from '../thumbnail/thumbnail.module';
 
@@ -13,6 +16,11 @@ import { ThumbnailModule } from '../thumbnail/thumbnail.module';
     PostTagModule,
     // forwardRef to handle potential circular dependency with ThumbnailModule
     forwardRef(() => ThumbnailModule),
+  ],
+  controllers: [
+    AlbumController,
+    AlbumCategoryController,
+    PublicAlbumController,
   ],
   providers: [
     AlbumService,
